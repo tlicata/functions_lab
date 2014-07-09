3a0df411200ca494bb2dc6124e63b434a1b73dad
Tim Licata <tlicata@hotpads.com>
2014-07-09 12:21:16 -0700

    add some comments to merge

--- merge.js
+++ merge.js
@@ -1,6 +1,14 @@
 var merge = function (arrayOne, arrayTwo) {
+    // Declare a variable that we will use to
+    // hold the newly merged array.
     var result = [];
 
+    // As long as neither array argument is empty,
+    // we need to compare the minimum number from
+    // each (which will be in the first position
+    // since we know the arrays are sorted). We
+    // remove the smallest element from its array
+    // and add it to our accumulated results array.
     while (arrayOne.length > 0 && arrayTwo.length > 0) {
         var smallest;
         if (arrayOne[0] < arrayTwo[0]) {
@@ -11,6 +19,9 @@ var merge = function (arrayOne, arrayTwo) {
         result.push(smallest);
     }
 
+    // Once at least one array is empty, we know we can
+    // take any remaining numbers from the other array
+    // and add them on to the end.
     if (arrayOne.length > 0) {
         result = result.concat(arrayOne);
     } else if (arrayTwo.length > 0) {
