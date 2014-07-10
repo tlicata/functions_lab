a9929ae54eafedad9f7b5bf09dacd7e1e0c14764
Tim Licata <tlicata@hotpads.com>
2014-07-10 12:04:48 -0700

    remove comments from merge_shortened.js to make it really short

--- merge_shortened.js
+++ merge_shortened.js
@@ -1,22 +1,8 @@
-// Merge two sorted arrays into one longer sorted array.
 var merge = function (arrayOne, arrayTwo) {
-    // Declare a variable that we will use to
-    // hold the newly merged array.
     var result = [];
-
-    // As long as neither array argument is empty,
-    // we need to compare the minimum number from
-    // each (which will be in the first position
-    // since we know the arrays are sorted). We
-    // remove the smallest element from its array
-    // and add it to our accumulated results array.
     while (arrayOne.length && arrayTwo.length) {
         result.push(arrayOne[0] < arrayTwo[0] ? arrayOne.shift() : arrayTwo.shift());
     }
-
-    // Once at least one array is empty, we know we can
-    // take any remaining numbers from the other array
-    // and add them on to the end.
     return result.concat(arrayOne).concat(arrayTwo);
 };
 
