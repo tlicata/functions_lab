d2da3f0e82a6fc888d1413fa18ccd5aa12a5e6ab
Tim Licata <tlicata@hotpads.com>
2014-07-10 11:56:47 -0700

    shorten merge solution

--- merge.js
+++ merge.js
@@ -11,25 +11,17 @@ var merge = function (arrayOne, arrayTwo) {
     // remove the smallest element from its array
     // and add it to our accumulated results array.
     while (arrayOne.length > 0 && arrayTwo.length > 0) {
-        var smallest;
         if (arrayOne[0] < arrayTwo[0]) {
-            smallest = arrayOne.shift();
+            result.push(arrayOne.shift());
         } else {
-            smallest = arrayTwo.shift();
+            result.push(arrayTwo.shift());
         }
-        result.push(smallest);
     }
 
     // Once at least one array is empty, we know we can
     // take any remaining numbers from the other array
     // and add them on to the end.
-    if (arrayOne.length > 0) {
-        result = result.concat(arrayOne);
-    } else if (arrayTwo.length > 0) {
-        result = result.concat(arrayTwo);
-    }
-
-    return result;
+    return result.concat(arrayOne).concat(arrayTwo);
 };
 
 var arr1 = [3,6,11];
