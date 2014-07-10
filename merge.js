949fc9fc5e22c8ef84d3ca992a11dd03e709cc80
Tim Licata <tlicata@hotpads.com>
2014-07-10 11:59:05 -0700

    Revert "shorten merge solution"
    This reverts commit d2da3f0e82a6fc888d1413fa18ccd5aa12a5e6ab.
    
    The extra statements were for illustration purposes.

--- merge.js
+++ merge.js
@@ -11,17 +11,25 @@ var merge = function (arrayOne, arrayTwo) {
     // remove the smallest element from its array
     // and add it to our accumulated results array.
     while (arrayOne.length > 0 && arrayTwo.length > 0) {
+        var smallest;
         if (arrayOne[0] < arrayTwo[0]) {
-            result.push(arrayOne.shift());
+            smallest = arrayOne.shift();
         } else {
-            result.push(arrayTwo.shift());
+            smallest = arrayTwo.shift();
         }
+        result.push(smallest);
     }
 
     // Once at least one array is empty, we know we can
     // take any remaining numbers from the other array
     // and add them on to the end.
-    return result.concat(arrayOne).concat(arrayTwo);
+    if (arrayOne.length > 0) {
+        result = result.concat(arrayOne);
+    } else if (arrayTwo.length > 0) {
+        result = result.concat(arrayTwo);
+    }
+
+    return result;
 };
 
 var arr1 = [3,6,11];
